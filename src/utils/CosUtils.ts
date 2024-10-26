389f3806655ffedd85b27ef6d149eca145ef36cb
李智维 <13434615275@163.com>
2024-10-26 21:14:48 +0800

    feat: 引入 COS，并且添加创建 COS 实例逻辑，用于上传文件至 COS

---
+++ src/utils/CosUtils.ts
@@ -0,0 +1,29 @@
+import COS from "cos-js-sdk-v5";
+import { CosService, FileControllerService, Service } from '../../generated'
+
+export const cos = new COS({
+  // getAuthorization 必选参数
+  getAuthorization: function (options, callback) {
+    // 初始化时不会调用，只有调用 cos 方法（例如 cos.putObject）时才会进入
+    // 异步获取临时密钥
+    // 服务端 JS 和 PHP 例子：https://github.com/tencentyun/cos-js-sdk-v5/blob/master/server/
+    // 服务端其他语言参考 COS STS SDK ：https://github.com/tencentyun/qcloud-cos-sts-sdk
+    // STS 详细文档指引看：https://cloud.tencent.com/document/product/436/14048
+
+    CosService.getCosCredentials().then((res) => {
+      if (res.code !== 0) {
+        return;
+      }
+      const data = res.data;
+      const credentials = data.credentials;
+      callback({
+        TmpSecretId: credentials.tmpSecretId,
+        TmpSecretKey: credentials.tmpSecretKey,
+        SecurityToken: credentials.sessionToken,
+        // 建议返回服务器时间作为签名的开始时间，避免用户浏览器本地时间偏差过大导致签名错误
+        StartTime: data.startTime, // 时间戳，单位秒，如：1580000000
+        ExpiredTime: data.expiredTime, // 时间戳，单位秒，如：1580000000
+      });
+    });
+  },
+});
