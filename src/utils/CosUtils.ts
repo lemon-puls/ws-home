a66c98045b4a4def6c6113f9dd8bcd18a3cfc7c9
李智维 <13434615275@163.com>
2024-10-27 10:30:44 +0800

    feat: COS 工具类添加生成 UUID 方法，方便生成文件名

--- src/utils/CosUtils.ts
+++ src/utils/CosUtils.ts
@@ -28,3 +28,13 @@ export const cos = new COS({
     });
   },
 });
+
+
+export const generateUUID = () => {
+  // 生成随机的 UUID
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
