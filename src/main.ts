0192787ae2d42d2d0bda2918ddcaffd4b8f4900f
李智维 <13434615275@163.com>
2024-11-05 00:19:35 +0800

    feat: 引入 pinia 的持久化存储插件，实现状态持久化

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,22 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import "virtual:svg-icons-register";
-import "@/style/main.css";
+import 'virtual:svg-icons-register'
+import '@/style/main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
 app.use(router)
 app.use(ElementPlus)
-
 
 app.mount('#app')
