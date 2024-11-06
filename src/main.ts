036d6c9283948c3b05cf974f2a2f3e9cf00a7e5a
李智维 <13434615275@163.com>
2024-11-06 22:40:46 +0800

    feat: 实现在 axios 响应拦截器中对未登录响应统一处理，弹出登陆框

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import "@/plugins/axios";
+import '@/plugins/axios'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { useUserStore } from '@/stores/user'
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +12,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'virtual:svg-icons-register'
 import '@/style/main.css'
+import { setStoreGetter } from '@/plugins/axios'
 
 const app = createApp(App)
 
@@ -18,6 +20,10 @@ const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
+// @/plugins/axios 注入 useUserStore 值，使得其可以使用 userStore
+// 如果直接在在 @/plugins/axios 使用 useUserStore，有可能 pinia 还未初始化，导致报错
+setStoreGetter(() => useUserStore())
+
 app.use(router)
 app.use(ElementPlus)
 
--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,36 +1,53 @@
-// Add a request interceptor
-import axios from "axios";
+import axios from 'axios'
+import { BACKEND_RESPONSE_CODE } from '@/enume'
+import { useUserStore } from '@/stores/user'
+import { ElMessage } from 'element-plus'
 
-axios.defaults.withCredentials = true;
+// 创建一个获取 store 的函数
+let storeGetter: () => ReturnType<typeof useUserStore>
+// 由 main.ts 执行完 app.use(pinia) 后调用设置值
+// 设置获取 store 的函数
+export const setStoreGetter = (getter: () => ReturnType<typeof useUserStore>) => {
+  storeGetter = getter
+}
 
+axios.defaults.withCredentials = true
+
+// 请求拦截器
 axios.interceptors.request.use(
   function (config) {
-    // Do something before request is sent
-    // 添加token到请求头
-    const token = localStorage.getItem('ACCESS_TOKEN');
+    const token = localStorage.getItem('ACCESS_TOKEN')
     if (token) {
-      console.log('token', token);
-      config.headers["Authorization"] = "Bearer " + token;
+      config.headers['Authorization'] = 'Bearer ' + token
     }
-    return config;
+    return config
   },
   function (error) {
-    // Do something with request error
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
-// Add a response interceptor
+// 响应拦截器
 axios.interceptors.response.use(
   function (response) {
-    // console.log("响应", response);
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
+    const data = response.data
+
+    // 如果返回未登录状态码
+    if (data.code === BACKEND_RESPONSE_CODE.NotLogin) {
+      // 使用 getter 函数获取 store
+      const userStore = storeGetter?.()
+      if (userStore) {
+        userStore.logout()
+      }
+      // 明确返回 rejected promise
+      return Promise.reject({
+        code: BACKEND_RESPONSE_CODE.NotLogin,
+        message: '请先登录'
+      })
+    }
+    return response
   },
   function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
