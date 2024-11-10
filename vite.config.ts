78c4186dd94fd73e794dfef8bd72b084f6c5fd54
李智维 <13434615275@163.com>
2024-11-10 15:48:17 +0800

    feat: 把后端 API 代理地址和实际地址挪到 .env 配置文件中进行配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    createSvgIconsPlugin({
-      // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
-      // Specify symbolId format
-      symbolId: 'icon-[name]'
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      // 代理的路径
-      '/api': {
-        target: 'http://localhost:8080', // 目标服务器地址
-        changeOrigin: true, // 是否改变请求源
+export default defineConfig(({ command, mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      createSvgIconsPlugin({
+        // Specify the icon folder to be cached
+        iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
+        // Specify symbolId format
+        symbolId: 'icon-[name]'
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        // 代理的路径
+        '/api': {
+          target: env.VITE_API_PROXY_URL,
+          changeOrigin: true // 是否改变请求源
+        }
       }
     }
   }
