af73581092f19c200304901a14a71db05f03f1bc
李智维 <13434615275@163.com>
2024-12-02 00:31:38 +0800

    feat: 轮播图图片改为在 .env 文件中配置，后续优化为支持在后端配置

--- src/views/PhotoManagerView.vue
+++ src/views/PhotoManagerView.vue
@@ -10,11 +10,8 @@ import PhotoStatsDialog from '@/components/photoAlbum/PhotoStatsDialog.vue'
 
 const albumStore = useAlbumStore()
 
-const imgList = [
-  'https://th.bing.com/th/id/OIP.YKoZzgmubNBxQ8j-mmoTKAHaEK?w=329&h=185&c=7&r=0&o=5&dpr=1.3&pid=1.7',
-  'https://www.bing.com/images/search?view=detailV2&ccid=neU%2flyZX&id=8EA47C03DC5EF4998C0487D376C492D2577760B1&thid=OIP.neU_lyZXZpazGKjZXAlGywHaEK&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.9de53f9726576696b318a8d95c0946cb%3frik%3dsWB3V9KSxHbThw%26riu%3dhttp%253a%252f%252fpic.bizhi360.com%252fbbpic%252f95%252f9995_1.jpg%26ehk%3dGcPUjJED69TBvg9XxQr2klzDzfRsQWhAfLKlIAUWHJQ%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=1080&expw=1920&q=%e9%a3%8e%e6%99%af%e5%9b%be%e7%89%87%e5%a4%a7%e5%85%a8&simid=608019807869538389&FORM=IRPRST&ck=B8EC61E11CDC8E8E871D9CA1B5A48827&selectedIndex=3&itb=0',
-  'https://www.bing.com/images/search?view=detailV2&ccid=%2b34ZOx3w&id=3A928219641560C664840FA90E892701FC901BFB&thid=OIP.-34ZOx3wP4KeZnKue4L28gHaEo&mediaurl=https%3a%2f%2fwww.2008php.com%2f2011_Website_appreciate%2f2011-09-23%2f20110923163645.jpg&exph=900&expw=1440&q=%e9%a3%8e%e6%99%af%e5%9b%be%e7%89%87%e5%a4%a7%e5%85%a8&simid=608040269066018205&FORM=IRPRST&ck=7D164F69A6DCCDAEF44BAC3D759B72B2&selectedIndex=5&itb=0'
-]
+// 从环境变量获取轮播图配置
+const imgList = import.meta.env.VITE_CAROUSEL_IMAGES?.split(',') || []
 
 // 相册列表
 const albumList = ref<vo_AlbumVO[]>([])
